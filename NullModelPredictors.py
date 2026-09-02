95b953ec18b65d4c8d9ce72fd6f47f9caf9f31e5
Luke Donaldson <ldonaldson070@gmail.com>
2026-09-02 16:27:38 -0600

    Null regressor model is now finished

--- NullModelPredictors.py
+++ NullModelPredictors.py
@@ -21,8 +21,6 @@ from collections import Counter
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 
-
-
 columns = [
     'buying',
     'maint',
@@ -56,22 +54,54 @@ class NullClassifier:
         return [self.prediction] * len(X)
 
 
-model = NullClassifier()
-model.fit(y_train)
-predictions = model.predict(X_test)
+ClassifierModel = NullClassifier()
+ClassifierModel.fit(y_train)
+ClassifierPredictions = ClassifierModel.predict(X_test)
 
-accuracy = accuracy_score(y_test, predictions)
-print("Null Model Accuracy: ",round(accuracy * 100, 2), "%")#appears to be most accurate at around a 50/50 train-test-split
+ClassifierAccuracy = accuracy_score(y_test, ClassifierPredictions)
+print("Null Model Accuracy: ",round(ClassifierAccuracy * 100, 2), "%")#appears to be most accurate at around a 50/50 train-test-split
+
+#------------------------------
 
 
-class NullRegresser:
+columns = [
+    'Sex',
+    'Length',
+    'Diameter',
+    'Height',
+    'Whole weight',
+    'Shucked weight',
+    'Visceral weight',
+    'Shell weight',
+    'Rings'
+]
+
+df = pd.read_csv('datasets/abalone.data',names=columns)
+
+#X is features, y is target
+D = df.drop('Rings',axis=1)
+g = df['Rings']
+
+#print(df.head())
+#print(df.shape)
+#print(y.value_counts())
+
+D_train, D_test, g_train, g_test = train_test_split(D, g, test_size=0.6, random_state=808)
+
+class NullRegressor:
     def __init__(self):
         self.prediction = None
 
-    def fit(self, y):
-        self.prediction = sum(y) / len(y)
+    def fit(self, g):
+        self.prediction = sum(g) / len(g)
 
-    def predict(self, X):
-        return [self.prediction] * len(X)
+    def predict(self, D):
+        return [self.prediction] * len(D)
 
 
+RegressorModel = NullClassifier()
+RegressorModel.fit(g_train)
+RegressorPredictions = RegressorModel.predict(D_test)
+
+RegressorAccuracy = accuracy_score(g_test, RegressorPredictions)
+print("Null Model Accuracy: ",round(RegressorAccuracy * 100, 2), "%")#appears to be most accurate at around a 40/60 train-test-split
